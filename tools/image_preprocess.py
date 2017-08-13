0288fac88d49dd54a663feb936cfc2656caa7abb
max <huzehua@outlook.com>
2017-08-13 17:10:22 +0800

    preprocessing tools

--- tools/image_preprocess.py
+++ tools/image_preprocess.py
@@ -16,7 +16,7 @@ def load_image(path):
     assert (0 <= img).all() and (img <= 1.0).all()
     # print "Original Image Shape: ", img.shape
     # we crop image from center
-    short_edge = min(img.shape[:2])
+    short_edge = min(img.shape[:2])     #对于宽和高不一样的图片，按最短边将其裁剪为正方形
     yy = int((img.shape[0] - short_edge) / 2)
     xx = int((img.shape[1] - short_edge) / 2)
     crop_img = img[yy: yy + short_edge, xx: xx + short_edge]
